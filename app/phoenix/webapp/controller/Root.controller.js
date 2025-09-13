41e458c577c8c2451a53eeb09b8a70cb6206abf1
kr-ank-it <ankit.kr9819@gmail.com>
2025-09-13 10:08:53 +0000

    OData connection and dashboard screen for Ui5 app

--- app/phoenix/webapp/controller/Root.controller.js
+++ app/phoenix/webapp/controller/Root.controller.js
@@ -1,10 +1,39 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    'sap/ui/model/json/JSONModel'
+], (Controller, JSONModel) => {
     "use strict";
 
     return Controller.extend("phoenix.controller.Root", {
         onInit() {
+            
+            let aData = [
+                {
+                    key:"Products",
+                    value: "Products"
+                },
+                {
+                    key:"Categories",
+                    value: "Categories"
+                },
+                {
+                    key:"Customers",
+                    value: "Customers"
+                },
+                {
+                    key:"Orders",
+                    value: "Orders"
+                },
+                {
+                    key:"Suppliers",
+                    value: "Suppliers"
+                }
+            ];
+            let oOptionsModel = new JSONModel(aData);
+            this.getView().setModel(oOptionsModel, "oOptionsModel");
+        },
+        onShowProducts(){
+            console.log("Showing products");
         }
     });
 });
